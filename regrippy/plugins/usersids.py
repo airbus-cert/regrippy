dfa4c1473cc77c5a51af685aefc7cc42ae0edfa8
Tim Taylor <timtaylor3@yahoo.com>
2019-04-19 07:41:50 -0500

    Create usersids.py

---
+++ regrippy/plugins/usersids.py
@@ -0,0 +1,39 @@
+from regrippy import BasePlugin, PluginResult, mactime
+
+
+class Plugin(BasePlugin):
+    """Returns the user names with sids"""
+
+    __REGHIVE__ = "SOFTWARE"
+
+    def run(self):
+        path = r"Microsoft\Windows NT\CurrentVersion\ProfileList"
+        key = self.open_key(path)
+        if not key:
+            print('Key not found')
+            return
+
+        sid_list = list()
+
+        for v in key.subkeys():
+            sid_list.append(v.name())
+
+        for sid in sid_list:
+            sid_path = path + '\\' + sid
+            key2 = self.open_key(sid_path)
+
+            if not key2:
+                continue
+
+            for entry in key2.values():
+                if entry.name() == "ProfileImagePath":
+                    res = PluginResult(key=key, value=entry)
+                    user_name = entry.value().split('\\')[-1]
+                    res.custom['value'] = '{}:\t{}'.format(user_name, key2.name())
+                    yield res
+
+    def display_human(self, result):
+        print(result.custom['value'])
+
+    def display_machine(self, result):
+        print(mactime(name=result.custom['value'], mtime=result.mtime))
