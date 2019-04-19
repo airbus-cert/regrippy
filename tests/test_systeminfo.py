5cf00826a11f8b586c9540dd2e644c54089930e8
Tim Taylor <timtaylor3@yahoo.com>
2019-04-19 15:52:08 -0500

    Initial Commit

--- tests/test_systeminfo.py
+++ tests/test_systeminfo.py
@@ -4,68 +4,80 @@ import pytest
 
 from regrippy.plugins.systeminfo import Plugin as plugin
 
+
+@pytest.fixture
+def mock_software():
+    key = RegistryKeyMock.build("Microsoft\\Windows NT\\CurrentVersion")
+    reg = RegistryMock("SOFTWARE", "software", key.root())
+
+    key_install_date = RegistryKeyMock("InstallDate", key)
+    key_owner = RegistryKeyMock("RegisteredOwner", key)
+    key_product_name = RegistryKeyMock("ProductName", key)
+
+    val1 = RegistryValueMock("InstallDate", 1523364100, RegBin)
+    val2 = RegistryValueMock("RegisteredOwner", "Some Dude", RegSZ)
+    val3 = RegistryValueMock("ProductName", "Windows 7 Professional", RegSZ)
+
+    key.add_child(key_install_date)
+    key.add_child(key_owner)
+    key.add_child(key_product_name)
+
+    key.add_value(val1)
+    key.add_value(val2)
+    key.add_value(val3)
+
+    return reg
+
+
 @pytest.fixture
 def mock_system():
-    def mock_reg():
+    key = RegistryKeyMock.build("ControlSet001")
+    reg = RegistryMock("SYSTEM", "system", key.root())
+    reg.set_ccs(1)
 
-        key = RegistryKeyMock.build("ControlSet001")
+    key_control = RegistryKeyMock("Control", key)
+    key.add_child(key_control)
 
-        key_hostname = RegistryKeyMock("Control\\ComputerName\\ComputerName", key)
-        key_shutdowntime = RegistryKeyMock("Control\\Windows", key)
-        key_ipaddress = RegistryKeyMock("Services\\Tcpip\Parameters\\Interfaces", key)
+    key_hostname = RegistryKeyMock("ComputerName\\ComputerName", key)
+    key_shutdown = RegistryKeyMock("Windows\\ShutdownTime", key)
 
-        key.add_child(key_hostname)
-        key.add_child(key_shutdowntime)
-        key.add_child(key_dhcpipaddress)
-        key.add_child(key_ipaddress)
+    key.add_child(key_hostname)
+    key.add_child(key_shutdown)
 
-        val1 = RegistryValueMock(key_hostname, "TestPC", RegSZ)
-        val2 = RegistryValueMock(key_shutdowntime, b'\xe40\xa5!\xed\xf7\xd3\x01', RegBin)
-        val3 = RegistryValueMock(key_dhcpipaddress, "127.0.0.1", RegSZ)
-        val4 = RegistryValueMock(key_ipaddress, "127.0.0.2", RegSZ)
+    val1 = RegistryValueMock(key_hostname, "SomeTestPCName", RegSZ)
+    val2 = RegistryValueMock(key_shutdown, b'\xe40\xa5!\xed\xf7\xd3\x01', RegBin)
 
-        key.add_value(val1)
-        key.add_value(val2)
-        key.add_value(val3)
-        key.add_value(val4)
+    key.add_value(val1)
+    key.add_value(val2)
 
-        reg = RegistryMock("SYSTEM", "system", key.root())
-        reg.set_ccs(1)
+    key_interface = RegistryKeyMock("Services\\Tcpip\\Parameters\\Interfaces\\", key)
+    key.add_child(key_interface)
 
-def mock_software():
-    def mock_reg():
-        key = RegistryKeyMock.build("Microsoft\\Windows NT\\CurrentVersion")
+    key_dhcp_ipaddress = RegistryKeyMock("{456A6A17D-21FC-123F-A689-A846D86EC008}\\DhcpIPAddress", key_interface)
+    key_ip_address = RegistryKeyMock("{123ee342-7039-466e-9d20-806e6f6e6963}\\IPAddress", key_interface)
 
-        val1 = RegistryValueMock("InstallDate", b'\xe40\xa5!\xed\xf7\xd3\x01', RegBin)
-        val2 = RegistryValueMock("RegisteredOwner", "AnyOwner", RegSZ)
-        val3 = RegistryValueMock("ProductName", "Windows 7 Professional", RegSZ)
+    key.add_child(key_dhcp_ipaddress)
+    key.add_child(key_ip_address)
 
-        key.add_value(val1)
-        key.add_value(val2)
-        key.add_value(val3)
+    val3 = RegistryValueMock(key_dhcp_ipaddress, '127.0.0.1', RegSZ)
+    val4 = RegistryValueMock(key_ip_address, '127.0.0.2', RegSZ)
 
-        return reg
+    key.add_value(val3)
+    key.add_value(val4)
 
-    def test_systeminfo(mock_reg):
-        # SOFTWARE
-        # p = plugin(mock_software, LoggerMock(), "SOFTWARE", "-")
-        # results = list(p.run())
+    return reg
 
-        # assert (len(results) == 2), "There should be  results, one per key"
-        """
-        for r in results:
-            assert (r.value_name == "autorun"), f"Program label for {r.key_path} should be autorun"
-            assert (r.value_data == "evil.exe"), f"Program path for {r.keypath} should be evil.exe"
-        """
 
-        # SYSTEM
-        p = plugin(mock_system, LoggerMock(), "SYSTEM", "-")
-        results = list(p.run())
+def test_run(mock_system, mock_software ):
+    # SOFTWARE
+    p = plugin(mock_software, LoggerMock(), "SOFTWARE", "-")
+    results = list(p.run())
 
-        assert (len(results) == 4), "There should be 4 results, one per key"
-        """
-        for r in results:
-            assert (r.value_name == "autorun"), f"Program label for {r.key_path} should be autorun"
-            assert (r.value_data == "evil.exe"), f"Program path for {r.keypath} should be evil.exe"
-            assert (r.value_data == "evil.exe"), f"Program path for {r.keypath} should be evil.exe"
-        """
+    assert (len(results) == 3), "There should be  3 results."
+
+    # SYSTEM
+    p = plugin(mock_system, LoggerMock(), "SYSTEM", "-")
+    results = list(p.run())
+
+    assert (len(results) == 4), "There should be 4 results"
+
