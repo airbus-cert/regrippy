d5f01ea31e2a6d0df65642fe4fdab139bf32fbe1
timtaylor3 <timtaylor3@yahoo.com>
2020-03-06 14:13:41 -0600

    Initial commit with limited testing.  Work in Progress.

--- regrippy/plugins/vsc.py
+++ regrippy/plugins/vsc.py
@@ -1,10 +1,21 @@
 from regrippy import BasePlugin, PluginResult, mactime
-from Registry.RegistryParse import parse_windows_timestamp
-# Not tested
-
+# TODO:  Create Test Case
 
 class Plugin(BasePlugin):
     """Returns the computer's VSC Data"""
+
+    """
+    -------+----------------------------------------------------------------------------------------------------------------------------------+
+    | Value |                                                           Description                                                            |
+    +-------+----------------------------------------------------------------------------------------------------------------------------------+
+    |     0 | Boot: Loaded by kernel loader. Components of the driver stack for the boot (startup) volume must be loaded by the kernel loader. |
+    |     1 | System: Loaded by I/O subsystem. Specifies that the driver is loaded at kernel initialization.                                   |
+    |     2 | Automatic: Loaded by Service Control Manager. Specifies that the service is loaded or started automatically.                     |
+    |     3 | Manual: The service does not start until the user starts it manually, such as by using Services or Devices in Control Panel.     |
+    |     4 | Disabled: Specifies that the service should not be started.                                                                      |
+    +-------+----------------------------------------------------------------------------------------------------------------------------------+
+    """
+
     __REGHIVE__ = "SYSTEM"
 
     def run(self):
@@ -14,7 +25,7 @@ class Plugin(BasePlugin):
                  self.get_currentcontrolset_path() + r"\Services\VSS\VssAccessControl",
                  self.get_currentcontrolset_path() + r"\Control\BackupRestore\FilesNotToBackup",
                  self.get_currentcontrolset_path() + r"\Control\BackupRestore\FilesNotToSnapshot",
-                 self.get_currentcontrolset_path() + r"\Control\BackupRestore\FilesNotToRestore",
+                 self.get_currentcontrolset_path() + r"\Control\BackupRestore\FilesNotToRestore"
                  ]
 
         for path in paths:
@@ -40,6 +51,7 @@ class Plugin(BasePlugin):
 
     def display_machine(self, result):
         if "VSS" in result.custom['Path']:
-            print(mactime(name=f"{result.custom['Path']}\{result.value_name} {result.custom['Value']}", mtime=result.mtime))
+            print(mactime(name=f"{result.custom['Path']}\{result.value_name} {result.custom['Value']}",
+                          mtime=result.mtime))
         else:
             print(mactime(name=f"{result.custom['Path']}\{result.value_name}", mtime=result.mtime))
