76581eabfe2e4da1e8cb512ea8897fd3c9ab43f7
Simon Garrelou <simon.garrelou@airbus.com>
2020-07-02 16:14:37 +0200

    Add proxy plugin

---
+++ regrippy/plugins/proxy.py
@@ -0,0 +1,51 @@
+from regrippy import BasePlugin, mactime, PluginResult
+from Registry.Registry import RegistryValueNotFoundException
+
+
+class Plugin(BasePlugin):
+    """Extracts the user's proxy settings"""
+
+    __REGHIVE__ = "NTUSER.DAT"
+
+    def run(self):
+        k = self.open_key(r"Software\Microsoft\Windows\CurrentVersion\Internet Settings")
+        if not k:
+            return
+        
+        try:
+            r = PluginResult(key=k)
+
+            r.custom["type"] = "proxy"
+            r.custom["enabled"] = self.safe_value(k, "ProxyEnabled", 0) == 1
+            r.custom["proxy"] = self.safe_value(k, "ProxyServer", "N/A")
+            r.custom["exceptions"] = self.safe_value(k, "ProxyOverride", "")
+
+            yield r
+
+            r = PluginResult(key=k)
+            r.custom["type"] = "autoconfig"
+            r.custom["proxypac"] = self.safe_value(k, "AutoConfigURL", "N/A")
+
+            yield r
+        except RegistryValueNotFoundException as e:
+            self.warning("Error getting proxy info:", e)
+    
+    def display_human(self, r):
+        if r.custom["type"] == "proxy":
+            print("[PROXY]")
+            print(f"\tEnabled: {r.custom['enabled']}")
+            print(f"\tProxy URL: {r.custom['proxy']}")
+            print(f"\tExceptions: {r.custom['exceptions']}")
+        elif r.custom["type"] == "autoconfig":
+            print("[AUTO-CONFIG]")
+            print(f"\tProxyPac URL: {r.custom['proxypac']}")
+        print()
+    
+    def display_machine(self, r):
+        print(mactime(name="Proxy settings modified", mtime=r.mtime))
+
+    def safe_value(self, k, name, default):
+        for v in k.values():
+            if v.name() == name:
+                return v.value()
+        return default
---
+++ tests/test_proxy.py
@@ -0,0 +1,44 @@
+from .reg_mock import RegistryMock, RegistryKeyMock, RegistryValueMock, LoggerMock
+from Registry.Registry import RegSZ, RegDWord
+import pytest
+
+from regrippy.plugins.proxy import Plugin
+
+
+@pytest.fixture
+def mock_reg():
+    key = RegistryKeyMock.build(r"Software\Microsoft\Windows\CurrentVersion\Internet Settings")
+    reg = RegistryMock("NTUSER.DAT", "ntuser.dat", key.root())
+
+    # Add a normal proxy config
+    url = RegistryValueMock("ProxyServer", "172.16.2.34:8080", RegSZ)
+    enabled = RegistryValueMock("ProxyEnabled", 1, RegDWord)
+    exceptions = RegistryValueMock("ProxyOverride", "10.0.0.0/8", RegSZ)
+
+    key.add_value(url)
+    key.add_value(enabled)
+    key.add_value(exceptions)
+
+    # Add a Proxy.pac
+    pac = RegistryValueMock("AutoConfigURL", "http://proxy.internal.corp/proxy.pac", RegSZ)
+
+    key.add_value(pac)
+
+    return reg
+
+
+def test_proxy(mock_reg):
+    p = Plugin(mock_reg, LoggerMock, "NTUSER.DAT", "-")
+
+    results = list(p.run())
+    
+    assert len(results) == 2, "There should be 2 results: a simple proxy and a proxypac"
+    
+    proxy = [x for x in results if x.custom["type"] == "proxy"][0]
+    pac = [x for x in results if x.custom["type"] == "autoconfig"][0]
+
+    assert proxy.custom["enabled"], "Proxy should be enabled"
+    assert proxy.custom["proxy"] == "172.16.2.34:8080", "Proxy should be set to '172.16.2.34:8080'"
+    assert proxy.custom["exceptions"] == "10.0.0.0/8", "There should be a single exception"
+
+    assert pac.custom["proxypac"] == "http://proxy.internal.corp/proxy.pac", "The ProxyPAC should be properly configured"
