65f14d68adc677ac69c30c7f4f5c9fe345fe76d9
Simon Garrelou <simon.garrelou@airbus.com>
2020-07-02 16:14:56 +0200

    Add putty plugin

---
+++ regrippy/plugins/putty.py
@@ -0,0 +1,63 @@
+from regrippy import BasePlugin, mactime, PluginResult
+from Registry.Registry import RegistryKeyNotFoundException, RegistryValueNotFoundException
+
+
+class Plugin(BasePlugin):
+    """Lists information about PuTTY connections: saved public keys, saved sessions, etc."""
+
+    __REGHIVE__ = "NTUSER.DAT"
+
+    def run(self):
+        k = self.open_key(r"Software\SimonTatham\PuTTY")
+        if not k:
+            return
+        
+        # SSH host keys
+        try:
+            ssh_host_keys = k.subkey("SshHostKeys")
+            for v in ssh_host_keys.values():
+                name = v.name()
+                crypto, host = name.split("@")
+
+                r = PluginResult(key=ssh_host_keys, value=v)
+                r.custom["type"] = "sshkey"
+                r.custom["crypto"] = crypto
+                r.custom["host"] = host
+
+                yield r
+        except RegistryKeyNotFoundException:
+            self.warning("Could not find SshHostKeys subkey")
+        
+        # Saved sessions
+        try:
+            sessions = k.subkey("Sessions")
+            for sess in sessions.subkeys():
+                try:
+                    name = sess.name()
+                    host = sess.value("HostName").value()
+                    proto = sess.value("Protocol").value()
+
+                    r = PluginResult(key=sess)
+                    r.custom["type"] = "session"
+                    r.custom["host"] = host
+                    r.custom["protocol"] = proto
+
+                    yield r
+                except RegistryValueNotFoundException:
+                    self.warning(f"Malformed entry: {sess.name()}")
+        except RegistryKeyNotFoundException:
+            self.warning("Could not find Sessions subkey")
+        
+    def display_human(self, res):
+        if res.custom["type"] == "sshkey":
+            print(f"[SSH KEY] {res.custom['host']}\t(key type: {res.custom['crypto']})")
+        elif res.custom["type"] == "session":
+            print(f"[SESSION] \"{res.key_name}\" => {res.custom['host']}\t(protocol: {res.custom['protocol']})")
+        else:
+            self.error(f"Unknown result type: {res.custom['type']}")
+    
+    def display_machine(self, res):
+        if res.custom["type"] == "sshkey":
+            print(mactime(name=f"PuTTY SSH Key: {res.custom['host']} with {res.custom['crypto']}", mtime=res.mtime))
+        elif res.custom["type"] == "session":
+            print(mactime(name=f"PuTTY Session: {res.custom['host']} with {res.custom['protocol']}", mtime=res.mtime))
---
+++ tests/test_putty.py
@@ -0,0 +1,51 @@
+from .reg_mock import RegistryMock, RegistryKeyMock, RegistryValueMock, LoggerMock
+from Registry.Registry import RegSZ
+import pytest
+
+from regrippy.plugins.putty import Plugin
+
+
+@pytest.fixture
+def mock_reg():
+    key = RegistryKeyMock.build("Software\\SimonTatham\\PuTTY")
+    reg = RegistryMock("NTUSER.DAT", "ntuser.dat", key.root())
+
+    sessions = RegistryKeyMock("Sessions", key)
+    key.add_child(sessions)
+
+    # Sample session to my-ssh-server.com
+    sess_key = RegistryKeyMock("MySSH", sessions)
+    sessions.add_child(sess_key)
+
+    sess_key.add_value(RegistryValueMock("HostName", "my-ssh-server.com", RegSZ))
+    sess_key.add_value(RegistryValueMock("Protocol", "ssh", RegSZ))
+
+    # Host key to evil-server.eu
+    ssh_hosts = RegistryKeyMock("SshHostKeys", key)
+    key.add_child(ssh_hosts)
+
+    ssh_hosts.add_value(RegistryValueMock("ecdsa@22:evil-server.eu", "0xbadc00ffee", RegSZ))
+
+    return reg
+
+
+def test_putty(mock_reg):
+    p = Plugin(mock_reg, LoggerMock(), "NTUSER.DAT", "-")
+
+    results = list(p.run())
+
+    sessions = [x for x in results if x.custom["type"] == "session"]
+    host_keys = [x for x in results if x.custom["type"] == "sshkey"]
+
+    assert len(sessions) == 1, "There should be a single saved session"
+    assert len(host_keys) == 1, "There should be a single saved SSH key"
+
+    session = sessions[0]
+    host_key = host_keys[0]
+
+    assert session.key_name == "MySSH", "The session's name should be 'MySSH'"
+    assert session.custom["host"] == "my-ssh-server.com", "The session should point to my-ssh-server.com"
+    assert session.custom["protocol"] == "ssh", "The session should be using SSH"
+
+    assert host_key.custom["crypto"] == "ecdsa", "Host key should be detected as 'ecdsa'"
+    assert host_key.custom["host"] == "22:evil-server.eu", "Host for the saved key should be 'evil-server.eu'"
